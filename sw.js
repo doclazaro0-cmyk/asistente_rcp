6bde1c840e8cd17b9ff5998c5befb83db69f6815
doclazaro0-cmyk <doclazaro0@gmail.com>
2026-01-16 10:29:14 -0600

    sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'rcp-v1';
+const assets = [
+  './',
+  './index.html'
+];
+
+// Instalar y guardar en memoria
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Responder desde la memoria si no hay internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
